5090a7f1a683f79f35d07f3c3f82c526064253a4
Dravin <dravinnnnn@gmail.com>
2025-10-13 23:35:13 +0700

    Add files via upload

--- settings/config.js
+++ settings/config.js
@@ -1,1 +1,16 @@
+import fs from "fs";
+import chalk from "chalk";
 
+global.owner = ["6283122563863"];
+global.mode = false;
+
+global.namebotz = "Ess Bot !";
+global.packname = 'www.essentialsr.web.id.';
+global.nameown = "EssentialsR | Teams";
+global.author = 'https://www.github.com/DravinTempest';
+global.footer = "𝗍𝖾𝗅𝖾𝗀𝗋𝖺𝗆: @dr4vin";
+
+global.YouTube = "https://www.youtube.com/@essuniversalofc";
+global.GitHub = "https://github.com/DravinTempest";
+global.Telegram = "https://t.me/dr4vin";
+global.ChannelWA = "https://whatsapp.com/channel/0029VbApgwQGU3BJWSbXUF0R";
