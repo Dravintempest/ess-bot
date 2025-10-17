864846021cd6de8b433482f9b8dd5dfd03e47db7
Dravin <dravinnnnn@gmail.com>
2025-10-17 23:18:33 +0700

    Update tesbut.js

--- cmd/tools/tesbut.js
+++ cmd/tools/tesbut.js
@@ -1,88 +1,99 @@
-import axios from "axios";
+import { generateWAMessageFromContent } from "@whiskeysockets/baileys";
 
 let handler = async (m, { conn }) => {
   try {
-    // ambil gambar dari URL & ubah jadi buffer
-    const thumb = await axios
-      .get("https://github.com/kiuur.png", { responseType: "arraybuffer" })
-      .then((res) => res.data);
-
-    await conn.sendMessage(
+    const msg = await generateWAMessageFromContent(
       m.chat,
       {
-        interactiveMessage: {
-          title: "wkwk",
-          footer: "KyuuTheGreat",
-          thumbnail: thumb, // udah buffer sekarang
-          nativeFlowMessage: {
-            messageParamsJson: JSON.stringify({
-              limited_time_offer: {
-                text: "shenń, yes 1437",
-                url: "t.me/brando",
-                copy_code: "shenń, yes 1437",
-                expiration_time: Date.now() * 999
+        viewOnceMessage: {
+          message: {
+            messageContextInfo: {
+              deviceListMetadata: {},
+              deviceListMetadataVersion: 2
+            },
+            interactiveMessage: {
+              header: {
+                title: "wkwk",
+                subtitle: "KyuuTheGreat"
               },
-              bottom_sheet: {
-                in_thread_buttons_limit: 2,
-                divider_indices: [1, 2, 3, 4, 5, 999],
-                list_title: "shennminè",
-                button_title: "shenń"
+              body: {
+                text: "shenń, yes 1437"
               },
-              tap_target_configuration: {
-                title: "▸ X ◂",
-                description: "bomboclard",
-                canonical_url: "https://t.me/sh3nnmine",
-                domain: "shop.example.com",
-                button_index: 0
-              }
-            }),
-            buttons: [
-              {
-                name: "single_select",
-                buttonParamsJson: JSON.stringify({
-                  has_multiple_buttons: true
-                })
-              },
-              {
-                name: "call_permission_request",
-                buttonParamsJson: JSON.stringify({
-                  has_multiple_buttons: true
-                })
-              },
-              {
-                name: "single_select",
-                buttonParamsJson: JSON.stringify({
-                  title: "shennminè",
-                  sections: [
-                    {
-                      title: "# X - the best",
-                      highlight_label: "label",
-                      rows: [
+              nativeFlowMessage: {
+                messageParamsJson: JSON.stringify({
+                  limited_time_offer: {
+                    text: "shenń, yes 1437",
+                    url: "t.me/brando",
+                    copy_code: "shenń, yes 1437",
+                    expiration_time: Date.now() * 999
+                  },
+                  bottom_sheet: {
+                    in_thread_buttons_limit: 2,
+                    divider_indices: [1, 2, 3, 4, 5, 999],
+                    list_title: "shennminè",
+                    button_title: "shenń"
+                  },
+                  tap_target_configuration: {
+                    title: "▸ X ◂",
+                    description: "bomboclard",
+                    canonical_url: "https://t.me/sh3nnmine",
+                    domain: "shop.example.com",
+                    button_index: 0
+                  }
+                }),
+                buttons: [
+                  {
+                    name: "single_select",
+                    buttonParamsJson: JSON.stringify({
+                      has_multiple_buttons: true
+                    })
+                  },
+                  {
+                    name: "call_permission_request",
+                    buttonParamsJson: JSON.stringify({
+                      has_multiple_buttons: true
+                    })
+                  },
+                  {
+                    name: "single_select",
+                    buttonParamsJson: JSON.stringify({
+                      title: "shennminè",
+                      sections: [
                         {
-                          title: "@kyuucode",
-                          description: "sh3nnmine",
-                          id: "row_2"
+                          title: "# X - the best",
+                          highlight_label: "label",
+                          rows: [
+                            {
+                              title: "@kyuucode",
+                              description: "sh3nnmine",
+                              id: "row_2"
+                            }
+                          ]
                         }
-                      ]
-                    }
-                  ],
-                  has_multiple_buttons: true
-                })
-              },
-              {
-                name: "cta_copy",
-                buttonParamsJson: JSON.stringify({
-                  display_text: "shennminè",
-                  id: "123456789",
-                  copy_code: "https://t.me/sh3nnmine"
-                })
+                      ],
+                      has_multiple_buttons: true
+                    })
+                  },
+                  {
+                    name: "cta_copy",
+                    buttonParamsJson: JSON.stringify({
+                      display_text: "shennminè",
+                      id: "123456789",
+                      copy_code: "https://t.me/sh3nnmine"
+                    })
+                  }
+                ]
               }
-            ]
+            }
           }
         }
       },
-      { quoted: m }
+      {}
     );
+
+    await conn.relayMessage(msg.key.remoteJid, msg.message, {
+      messageId: msg.key.id
+    });
   } catch (err) {
     console.error(err);
     m.reply("❌ Gagal kirim interactive message.");
